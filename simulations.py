32b1068e82a45eb0d79ea51265a1f68adb347afe
EL-DARK-STALKOR <36673944+EL-DARK-STALKOR@users.noreply.github.com>
2018-03-26 01:37:06 -0300

    Add files via upload

--- simulations.py
+++ simulations.py
@@ -1,36 +1,39 @@
+from tkinter import Label
+from tkinter import StringVar
+from tkinter import Entry
+from tkinter import Tk
+from tkinter import LabelFrame
+from tkinter import END
+from tkinter import Button
 from tkinter import tix
-from tkinter import *
+import tkinter.font as tkFont
 
-
-"""commentaires cinq premières lignes de codes
-fenetre: creation d'une interface principale où se déroulent tous les évènements(actions)
-
-liste_buzz: dictionnaire qui à chaque clé(0, 1, 2,...) associe une équipe choisis par l'utilisateur.
-             Exemple: {0: "Togo", 1: "Sénégal}
-
-liste_equipe: liste qui contient toutes les différentes équipes qui ont été entrées au début de jeu
-
-
-equipe_Select_var: disons que c'est une liste qui contient tous les différentes variable des "Combobox" parce que
-à chaque combobox est associé une variable qui correspondant à la variable sélectionnée dans une liste de choix
+"""
+Code qui permet de créer des équipes et d'associer ces équipes à des touches du clavier("buzzers") et détermine
+la premiere personne ayant appuyé sur le buzz
 """
 
-fenetre = tix.Tk()
-fenetre.geometry("400x400")
-liste_buzz = {}
+main_window = tix.Tk()
+main_window.geometry("400x400")
+dict_buzz = {}
 liste_equipe = []
-equipe_Select_var = []
+equipe_select_var = []
 
-Label(fenetre, text="CRÉER EQUIPE").pack()
+Label(main_window, text="CRÉER EQUIPE").pack()
 
+
+color_font_entry = tkFont.Font(slant ='italic')
 value = StringVar()
-value.set("texte par défaut")
-entree = Entry(fenetre, width=30)
+entree = Entry(main_window, textvariable = value,  width=30, font = color_font_entry, bg = "yellow")
 entree.pack()
 
 
-#Fonction qui crée une nouvelle fenêtre avec la liste des différents équipes du jeu
-def Affiche_equipe():
+
+def affiche_equipe():
+    """
+    Fonction qui crée une nouvelle fenêtre avec la liste des différents équipes du jeu
+    :return:
+    """
     liste_equipe_affiche = Tk()
     for i in liste_equipe:
         label = Label(liste_equipe_affiche, text= i )
@@ -38,13 +41,17 @@ def Affiche_equipe():
     liste_equipe_affiche.mainloop()
 
 
-def Modifier_equipe():
+def modifier_equipe():
     pass
 
 
 
-#Fonction qui permet d'ajouter des équipes à la liste des équipes(liste_equipe) avec le bouton "Ajouter équipe"
+
 def add_equipe():
+    """
+    Fonction qui permet d'ajouter des équipes à la liste des équipes(liste_equipe) avec le bouton "Ajouter équipe"
+    :return:
+    """
     equipe = entree.get().strip()
     print(equipe)
     if len(list(equipe)) >= 1:
@@ -53,8 +60,13 @@ def add_equipe():
             entree.delete(0, END)
 
 
-#Fonction qui permet d'ajouter des équipes à la liste des équipes(liste_equipe) avec la touche "entrée"
+
 def addition_team(event):
+    """
+    Fonction qui permet d'ajouter des équipes à la liste des équipes(liste_equipe) avec la touche "entrée"
+    :param event:
+    :return:
+    """
     equipe = entree.get().strip()
     print(equipe)
     if len(list(equipe)) >= 1:
@@ -63,87 +75,112 @@ def addition_team(event):
             entree.delete(0, END)
 
 
-#Fonction qui pour chaque clés du dictionnaire liste_buzz(0, 1,2...) associe une valeur
-#sous forme de chaîne de caractère correspondant à une équipe
-def Association_de_buzz(evt):
-    tuy = 0
-    for i in equipe_Select_var:
-        liste_buzz[tuy] = i.get()
-        tuy += 1
 
-#deuxième fenetre où se déroule l'association des équipes à un buzzer
-def deuxieme_fenetre():
-    for c in fenetre.winfo_children():
+def buzz_association(evt):
+    """
+    Fonction qui pour chaque clés du dictionnaire liste_buzz(0, 1,2...) associe une valeur
+    sous forme de chaîne de caractère correspondant à une équipe
+    :param evt:
+    :return:
+    """
+    indice_dict = 0
+    for i in equipe_select_var:
+        dict_buzz[indice_dict] = i.get()
+        indice_dict += 1
+
+
+def window_buzz_association():
+    """
+    deuxième fenetre où se déroule l'association des équipes à un buzzer
+    :return:
+    """
+    for c in main_window.winfo_children():
         c.destroy()
-    l = LabelFrame(fenetre, padx=35, pady=15)
-    l.pack(fill="both", expand="yes")
+    label_frame = LabelFrame(main_window, padx=35, pady=15)
+    label_frame.pack(fill="both", expand="yes")
 
-    Label(l, text="LIER ÉQUIPE").pack()
-    Label(fenetre, text = "Buzzers").pack()
+    Label(label_frame, text="LIER ÉQUIPE").pack()
+    Label(main_window, text ="Buzzers").pack()
     a = 90
 
     for n in range(0, (len(liste_equipe))):
         nom_buzz = "Buzz0{}".format(str(n))
-        Label(fenetre, text= nom_buzz).place(x = 45, y= a)
-        equipe_Select_var.append((tix.StringVar()))
-        Selection_equipe = tix.ComboBox(variable=equipe_Select_var[n], editable= True, dropdown=1,command = Association_de_buzz)
-        Selection_equipe.entry.config(state='readonly')
+        Label(main_window, text= nom_buzz).place(x = 45, y= a)
+        equipe_select_var.append((tix.StringVar()))
+        selection_equipe = tix.ComboBox(variable=equipe_select_var[n], editable= True, dropdown=1, command = buzz_association)
+        selection_equipe.entry.config(state='readonly', bg = "red")
 
-        yop = 0
+        i = 0
         for j in liste_equipe:
-            Selection_equipe.insert(yop, j)
-            yop+= 1
+            selection_equipe.insert(i, j)
+            i+= 1
 
-        Selection_equipe.pack()
-        Selection_equipe.place(x= 100, y= a)
+        selection_equipe.pack()
+        selection_equipe.place(x= 100, y= a)
         a += 55
-    bouton6 = Button(fenetre, text="Lister équipe", command=Affiche_equipe)
-    bouton6.pack()
-    bouton8 = Button(fenetre, text="Modifier équipe", command=Modifier_equipe)
-    bouton8.pack()
-    bouton4 = Button(fenetre, text="Continuer", command=troisieme_fenetre)
-    bouton4.pack()
+    btn_liste_equipe = Button(main_window, text="Lister équipe", command=affiche_equipe)
+    btn_liste_equipe.pack()
+    btn_modify_team = Button(main_window, text="Modifier équipe", command=modifier_equipe)
+    btn_modify_team.pack()
+    btn_continuer= Button(main_window, text="Continuer", command=window_declare_winner)
+    btn_continuer.pack()
 
 
-#derniere fenetre qui vérifie constamment si un joueur a appuyé sur une touche et désigne le gagnant
-def troisieme_fenetre():
-    for c in fenetre.winfo_children():
+
+def window_declare_winner():
+    """
+    derniere fenetre qui vérifie constamment si un joueur a appuyé sur une touche et désigne le gagnant
+    :return:
+    """
+    for c in main_window.winfo_children():
         c.destroy()
-    l = LabelFrame(fenetre, padx=45, pady=25)
-    l.pack(fill="both", expand="yes")
-    Label(l, text="EN ATTENTE DU BUZZ.....").pack()
+    label_frame = LabelFrame(main_window, padx=45, pady=25)
+    label_frame.pack(fill="both", expand="yes")
+    Label(label_frame, text="EN ATTENTE DU BUZZ.....").pack()
 
-    #fonction qui "agit" suite à une touche appuyé et qui affiche l'équipe premier au buzz
+
     def on_key_press(event):
-        fenetre.unbind("<KeyPress>")
-        dictop = [i for i in liste_buzz.keys()]
-        if int(event.char) in dictop:
+        """
+        fonction callback qui agit  suite à une touche appuyé et qui affiche l'équipe premier au buzz
+        :param event:
+        :return:
+        """
+        main_window.unbind("<KeyPress>")
+        dictop = [i for i in dict_buzz.keys()]
+        try:
+            if int(event.char) in dictop:
 
-            for c in fenetre.winfo_children():
-                c.destroy()
-            Label(fenetre, text="PREMIER AU BUZZ ").pack()
-            Label(fenetre, text=liste_buzz[int(event.char)]).pack()
+                    for c in main_window.winfo_children():
+                        c.destroy()
+                    Label(main_window, text="PREMIER AU BUZZ ").pack()
+                    Label(main_window, text=dict_buzz[int(event.char)]).pack()
 
-            bouton5 = Button(fenetre, text="Continuer", command=troisieme_fenetre)
-            bouton5.pack()
-            bouton7 = Button(fenetre, text="Lister équipe", command=Affiche_equipe)
-            bouton7.pack()
+                    btn_continuer = Button(main_window, text="Continuer", command=window_declare_winner)
+                    btn_continuer.pack()
+                    btn_liste_equipe = Button(main_window, text="Lister équipe", command=affiche_equipe)
+                    btn_liste_equipe.pack()
+            else:
+                window_declare_winner()
+        except:
+            window_declare_winner()
 
-    fenetre.bind("<KeyPress>", on_key_press)
-
-
-
-bouton1 = Button(fenetre, text="Ajouter équipe", command= add_equipe)
-bouton1.pack()
-
-fenetre.bind("<Return>", addition_team)
-
-bouton2 = Button(fenetre, text="Lister équipe", command= Affiche_equipe)
-bouton2.pack()
-
-bouton3 = Button(fenetre, text="Terminer", command = deuxieme_fenetre)
-bouton3.pack()
+    main_window.bind("<KeyPress>", on_key_press)
 
 
 
-fenetre.mainloop()
+btn_ajouter_equipe = Button(main_window, text="Ajouter équipe", command= add_equipe)
+btn_ajouter_equipe.pack()
+
+main_window.bind("<Return>", addition_team)
+
+btn_liste_equipe = Button(main_window, text="Lister équipe", command= affiche_equipe)
+btn_liste_equipe.pack()
+
+btn_terminer = Button(main_window, text="Terminer", command = window_buzz_association)
+btn_terminer.pack()
+
+
+
+
+
+main_window.mainloop()
