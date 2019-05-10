4df2aad55f5be3bf434b74778f4512ec42bb83ee
Your Name <you@example.com>
2019-05-10 12:16:59 -0700

    elvis

---
+++ ulaval_web.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome()
+driver.get("https://secure.sas.ulaval.ca/rtpeps/Account/Login?ReturnUrl=%2Frtpeps%2FReservation")
+assert "Se connecter - PEPS - Réservation de terrains" in driver.title
+elem = driver.find_element_by_id("Email")
+elem.clear()
+elem.send_keys("elvisametolo@gmail.com")
+elem = driver.find_element_by_id("Password")
+elem.clear()
+elem.send_keys("Bouteille20", Keys.ENTER)
+time.sleep(5)
+
+driver.find_elements_by_class_name("square")[0].click()
+
+
+
+
+driver.refresh()
+time.sleep(5)
+assert "No results found." not in driver.page_source
+driver.close()
